7da3e2b9374a04de2c89a25ff7be53f15fd474c6
sanvean74 <dlambertpdx@gmail.com>
2019-07-17 08:58:03 -0700

    tests passing

---
+++ src/guess.js
@@ -0,0 +1,12 @@
+//import compareNumbers from './make-guess.js';
+
+// let correctNumber = 13;
+
+const userGuessInput = document.getElementById('userGuess');
+const submitUserGuess = document.getElementById('submitGuess');
+
+submitUserGuess.addEventListener('click', () => {
+    return userGuessInput;
+});
+// eslint-disable-next-line no-console
+console.log(submitUserGuess)
---
+++ test/isYes.test.js
@@ -0,0 +1,32 @@
+import isYes from '../src/is-yes.js';
+const test = QUnit.test;
+
+test('yes is yes', (assert) => {
+    //Arrange
+    // Set up your parameters and expectations
+    const answer = 'yes';
+    const expected = true;
+
+    //Act
+    // Call the function you're testing and set the result to a const
+    const result = isYes(answer);
+
+    //Assert
+    assert.equal(result, expected);
+});
+
+test('y is yes', (assert) => {
+    //Arrange
+    // Set up your parameters and expectations
+    const answer = 'y';
+    const expected = true;
+
+    //Act
+    // Call the function you're testing and set the result to a const
+    const result = isYes(answer);
+
+    //Assert
+    assert.equal(result, expected);
+});
+
+// write 'no' response test
