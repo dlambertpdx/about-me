3f79bedf415211bef47823f8b5d0bbf33f6c40ed
sanvean74 <dlambertpdx@gmail.com>
2019-07-17 10:32:59 -0700

    compare numbers working

--- src/guess.js
+++ src/guess.js
@@ -1,12 +1,33 @@
-//import compareNumbers from './make-guess.js';
+/* eslint-disable no-console */
+/* eslint-disable no-unused-vars */
+// What do I need to import from other js files?
+import compareNumbers from './make-guess.js';
 
-// let correctNumber = 13;
+// What state do I need to track?
+let randomNumber = Math.floor(Math.random() * 20) + 1;
+let guessCount = 1;
 
-const userGuessInput = document.getElementById('userGuess');
-const submitUserGuess = document.getElementById('submitGuess');
+console.log('random number: ' + randomNumber);
 
-submitUserGuess.addEventListener('click', () => {
-    return userGuessInput;
+// What do I need from the DOM (elements)
+const guessInput = document.getElementById('userGuess');
+const guessSubmit = document.getElementById('submitGuess');
+const guessResult = document.getElementById('guessResult');
+const correctGuess = document.getElementById('correctResult');
+const tooLowOrHigh = document.getElementById('tooLowTooHigh');
+
+// What events do I need to listen to?
+guessSubmit.addEventListener('click', () => {
+    let userGuess = Number(guessInput.value);
+    console.log(userGuess);
+    if(userGuess > randomNumber) {
+        tooLowOrHigh.textContent = 'Sorry, your guess is too high!';
+    }
+    else if(userGuess < randomNumber) {
+        tooLowOrHigh.textContent = 'Sorry, your guess is too low!';
+    }
+    else {
+        correctGuess.textContent = 'You got it!';
+    }
+
 });
-// eslint-disable-next-line no-console
-console.log(submitUserGuess)
