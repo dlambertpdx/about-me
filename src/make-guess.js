4f069cfb63c24f8287a40b946c72d6304f0f73bf
sanvean74 <dlambertpdx@gmail.com>
2019-07-17 12:06:08 -0700

    fixed travis errors

--- src/make-guess.js
+++ src/make-guess.js
@@ -1,9 +1,9 @@
 function compareNumbers(guess, correctNumber) {
     // something to test
-    if (guess === correctNumber) {
+    if(guess === correctNumber) {
         return 0;
     }
-    else if (guess >= correctNumber) {
+    else if(guess >= correctNumber) {
         return 1;
     } else {
         return -1;
