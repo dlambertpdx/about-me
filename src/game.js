7875cf56a1f1512d4cd39c090b93dea523c6fdff
sanvean74 <dlambertpdx@gmail.com>
2019-07-18 12:34:48 -0700

    fixe some things but broke other things... :(

--- src/game.js
+++ src/game.js
@@ -2,10 +2,11 @@ import getPlay from './get-play.js';
 
 // Computer Choice
 const playGame = document.getElementById('play-game');
-const computerRock = document.getElementById('computer-rock');
-const computerPaper = document.getElementById('computer-paper');
-const computerScissors = document.getElementById('computer-scissors');
+// const computerRock = document.getElementById('computer-rock');
+// const computerPaper = document.getElementById('computer-paper');
+// const computerScissors = document.getElementById('computer-scissors');
 // User Choice
+const computerChoice = document.getElementById('computer-choice');
 const chooseRock = document.getElementById('choose-rock');
 const choosePaper = document.getElementById('choose-paper');
 const chooseScissors = document.getElementById('choose-scissors');
@@ -20,47 +21,43 @@ let losses = 0;
 playGame.addEventListener('click', () => {
     const computerMove = getPlay(); // random number function
     const userMove = document.querySelector('input:checked');
-
-    console.log('user chose: ' + userMove.value);
-
-    console.log('computer chose:' + computerMove);
-
-
+    computerChoice.classList.remove('invisible');
+    const src = 'assets/RPS/' + computerMove + '.jpg';
+    computerChoice.src = src;
+    console.log('you chose ' + userMove.value + ' & computer chose ' + computerMove);
 
     // game logic
-    if(computerMove === userMove) {
-        return message.textContent = 'It\'s a tie!';
+    if(computerMove.value === userMove.value) {
+        message.textContent = 'It\'s a tie!';
     }
-    if(userMove === chooseRock) {
+    if(userMove.value === chooseRock) {
         if(computerMove === computerMove.paper) {
             message.textContent = 'You lost!';
             losses++;
             lossCount.textContent = losses;
-            computerPaper.classList.remove('invisible');
         } else {
             message.textContent = 'You won!';
             wins++;
             winCount.textContent = wins;
         }
     }
-    if(userMove === choosePaper) {
+    if(userMove.value === choosePaper) {
         if(computerMove === computerMove.scissors) {
             message.textContent = 'You lost!';
             losses++;
             lossCount.textContent = losses;
-            computerScissors.classList.remove('invisible');
         } else {
             message.textContent = 'You won!';
             wins++;
             winCount.textContent = wins;
         }
     }
-    if(userMove === chooseScissors) {
+    if(userMove.value === chooseScissors) {
         if(computerMove === computerMove.rock) {
             message.textContent = 'You lost!';
             losses++;
             lossCount.textContent = losses;
-            computerRock.classList.remove('invisible');
+
         } else {
             message.textContent = 'You won!';
             wins++;
