58ab70df681431958c3eb27f5661b1d6eb1340be
sanvean74 <dlambertpdx@gmail.com>
2019-07-18 10:37:16 -0700

    update to wins and losses count

--- src/game.js
+++ src/game.js
@@ -5,45 +5,61 @@ const playGame = document.getElementById('play-game');
 const computerRock = document.getElementById('computer-rock');
 const computerPaper = document.getElementById('computer-paper');
 const computerScissors = document.getElementById('computer-scissors');
-
 // User Choice
 const chooseRock = document.getElementById('choose-rock');
 const choosePaper = document.getElementById('choose-paper');
 const chooseScissors = document.getElementById('choose-scissors');
-
 // Result
 const winCount = document.getElementById('win-count');
 const lossCount = document.getElementById('loss-count');
 const message = document.getElementById('message');
 
+let wins = 0;
+let losses = 0;
+
 playGame.addEventListener('click', () => {
     const computerMove = getPlay(); // random number function
     const userMove = document.querySelector('input:checked');
+    userMove.textContent = 'You chose ' + userMove;
 
     // game logic
     if(computerMove === userMove) {
-        return 'It\'s a tie!';
+        return message.textContent = 'It\'s a tie!';
     }
     if(userMove === chooseRock) {
         if(computerMove === computerPaper) {
-            return 'You lose!';
+            message.textContent = 'You lost!';
+            losses++;
+            lossCount.textContent = losses;
         } else {
-            return 'You win!';
+            message.textContent = 'You won!';
+            wins++;
+            winCount.textContent = wins;
         }
     }
     if(userMove === choosePaper) {
         if(computerMove === computerScissors) {
-            return 'You lose!';
+            message.textContent = 'You lost!';
+            losses++;
+            lossCount.textContent = losses;
         } else {
-            return 'You win!';
+            message.textContent = 'You won!';
+            wins++;
+            winCount.textContent = wins;
         }
     }
     if(userMove === chooseScissors) {
         if(computerMove === computerRock) {
-            return 'You lose!';
+            message.textContent = 'You lost!';
+            losses++;
+            lossCount.textContent = losses;
         } else {
-            return 'You win!';
+            message.textContent = 'You won!';
+            wins++;
+            winCount.textContent = wins;
         }
     }
+
+
 
 });
