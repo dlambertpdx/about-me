843e0fca28ebd19dbb1b3a2891f44714b26b1b1a
sanvean74 <dlambertpdx@gmail.com>
2019-07-18 14:09:59 -0700

    cleaned up spaghetti code

--- src/game.js
+++ src/game.js
@@ -1,70 +1,48 @@
 import getPlay from './get-play.js';
 
-// Computer Choice
 const playGame = document.getElementById('play-game');
-// const computerRock = document.getElementById('computer-rock');
-// const computerPaper = document.getElementById('computer-paper');
-// const computerScissors = document.getElementById('computer-scissors');
-// User Choice
 const computerChoice = document.getElementById('computer-choice');
-const chooseRock = document.getElementById('choose-rock');
-const choosePaper = document.getElementById('choose-paper');
-const chooseScissors = document.getElementById('choose-scissors');
-// Result
 const winCount = document.getElementById('win-count');
 const lossCount = document.getElementById('loss-count');
+const tieCount = document.getElementById('tie-count');
 const message = document.getElementById('message');
 
 let wins = 0;
 let losses = 0;
+let ties = 0;
 
 playGame.addEventListener('click', () => {
-    const computerMove = getPlay(); // random number function
-    const userMove = document.querySelector('input:checked');
+    const computerMove = getPlay();
+    const userMove = document.querySelector('input:checked').value;
+
     computerChoice.classList.remove('invisible');
     const src = 'assets/RPS/' + computerMove + '.jpg';
     computerChoice.src = src;
-    console.log('you chose ' + userMove.value + ' & computer chose ' + computerMove);
 
-    // game logic
-    if(computerMove.value === userMove.value) {
+    let score;
+
+    if(userMove === computerMove) {
+        score = 'tie';
+    } else if(userMove === 'rock' && computerMove === 'paper' ||
+    userMove === 'paper' && computerMove === 'scissors' ||
+    userMove === 'scissors' && computerMove === 'rock') {
+        score = 'lost';
+    } else {
+        score = 'won';
+    }
+
+    if(score === 'tie') {
         message.textContent = 'It\'s a tie!';
-    }
-    if(userMove.value === chooseRock) {
-        if(computerMove === computerMove.paper) {
-            message.textContent = 'You lost!';
-            losses++;
-            lossCount.textContent = losses;
-        } else {
-            message.textContent = 'You won!';
-            wins++;
-            winCount.textContent = wins;
-        }
-    }
-    if(userMove.value === choosePaper) {
-        if(computerMove === computerMove.scissors) {
-            message.textContent = 'You lost!';
-            losses++;
-            lossCount.textContent = losses;
-        } else {
-            message.textContent = 'You won!';
-            wins++;
-            winCount.textContent = wins;
-        }
-    }
-    if(userMove.value === chooseScissors) {
-        if(computerMove === computerMove.rock) {
-            message.textContent = 'You lost!';
-            losses++;
-            lossCount.textContent = losses;
+        ties++;
+        tieCount.textContent = ties;
+    } else if(score === 'lost') {
+        message.textContent = 'You lost!';
+        losses++;
+        lossCount.textContent = losses;
 
-        } else {
-            message.textContent = 'You won!';
-            wins++;
-            winCount.textContent = wins;
-        }
+    } else {
+        message.textContent = 'You won!';
+        wins++;
+        winCount.textContent = wins;
     }
-
-
-
 });
